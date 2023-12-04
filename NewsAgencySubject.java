5053e9611f88f63094d7a748defdc764ae8bac91
HannahCondada <142371011+HannahCondada@users.noreply.github.com>
2023-12-04 15:18:42 +0800

    Delete NewsAgencySubject.java

--- NewsAgencySubject.java
+++
@@ -1,7 +0,0 @@
-package ObserverPattern;
-
-    public interface NewsAgencySubject {
-        void subscribe(Subscriber subscriber);
-        void unSubscribe(Subscriber subscriber);
-        String notifySubscriber();
-}
